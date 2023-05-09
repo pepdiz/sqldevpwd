6439729ed22b70ccb22635af18ccdf30384d5dca
Pep <socialkuma@hotmail.com>
2023-05-09 18:00:21 +0200

    initial version
    outputs decrypted passwords to stdout

---
+++ sqldevpwd.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+import json
+import os
+import argparse
+import base64
+import hashlib
+from Cryptodome.Cipher import AES
+
+VERSION="1.0"
+
+def decryptpwd(encryptedpwd, decryptionkey):
+    b64pwd = base64.b64decode(encryptedpwd)
+    iv, pwd = b64pwd[:16], b64pwd[16:]    
+    
+    key = hashlib.pbkdf2_hmac("sha256", decryptionkey.encode(), b'\x06\xb6a#=h2\xb8', 5000)
+    
+    try:
+        crypter = AES.new(key, AES.MODE_CBC, iv)
+        dpwd = crypter.decrypt(pwd)
+        decrypted = dpwd[:-dpwd[-1]].decode('utf-8')      
+    except:
+        decrypted = '**ERROR** decrypting'
+    
+    return decrypted
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Prints to stdout decrypted passwords for connections in a SQL Developer 19.2+ export file in json format')
+    parser.add_argument('--version', action='version', version='%(prog)s ' + VERSION)
+    parser.add_argument("-k", dest="decryptkey", required=True,
+                  help="key to decrypt passwords, usually the export file encryption key or value of 'db.system.id' attribute in 'product-preferences.xml' file")
+    parser.add_argument("jsonfile", 
+                  help="reads FILE.json and decrypt all connection passwords in it", metavar="FILE.json")
+    
+  
+    args=parser.parse_args()
+    
+    try:
+        fic=os.path.realpath(args.jsonfile, strict=True)
+    except:
+        raise SystemExit ("json file " + args.jsonfile + " does not exist or cannot be found")
+        
+    with open(fic, "r") as sdevconn:
+        try:
+            datos=json.load(sdevconn)
+        except json.decoder.JSONDecodeError:
+            raise SystemExit ("problems reading json file")
+
+    for c in datos['connections']:
+        print (c['info']['ConnName'].ljust(50)," ",decryptpwd(c['info']['password'], args.decryptkey).ljust(30))                      
+
+
+if __name__ == "__main__" :
+    main()
